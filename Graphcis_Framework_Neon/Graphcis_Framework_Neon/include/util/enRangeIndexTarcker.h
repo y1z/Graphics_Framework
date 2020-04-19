03e9e352bdd5d851d5e8e5f5740fb3963d82f9da
YB <whyis2cool@gmail.com>
2020-04-18 21:30:10 -0500

    Finish 'enRangeIndexTracker'  .

--- Graphcis_Framework_Neon/Graphcis_Framework_Neon/include/util/enRangeIndexTarcker.h
+++ Graphcis_Framework_Neon/Graphcis_Framework_Neon/include/util/enRangeIndexTarcker.h
@@ -1,15 +1,141 @@
 #pragma once
-#include "en_defs.h"
 #include <type_traits>
+#include <array>
 
+  /**
+  * @brief : contains an index and if that index is being used.
+  * @tpara IndexType : the integer type of the index.
+  * @bug : no known bugs.
+  */
+template <class IndexType, std::enable_if_t<std::is_integral_v<IndexType>, int> = 0 >
+struct sIndexData 
+{
+  IndexType m_index;
+  bool m_isUsed = false;
+};
 
-template<class TypeOfTracker,
+  /**
+  * @brief : defines a range of indexes that go's from 0..n-1 and if they are being used.
+  * @tparam integralType : defines the type of the index.
+  * @tparam sizeOfRange : how big is the range.
+  * @bug : no known bugs.
+  */
+template<class integralType,
   size_t sizeOfRange,
-  std::enable_if_t<std::is_class_v<TypeOfTracker>
+  std::enable_if_t<std::is_integral_v<integralType>
   , int> = 0 >
-class baseRangeIndexTracker
+class enRangeIndexTracker
 {
 public:
 
+  using IndexType = sIndexData<integralType>;
+
+public:
+  enRangeIndexTracker()
+  {
+    integralType currentIndex = 0;
+    for( auto& index : m_indexes )
+    {
+      index.m_index = currentIndex;
+      currentIndex++;
+    }
+
+  }
+
+  enRangeIndexTracker(const enRangeIndexTracker& other) = delete;
+  enRangeIndexTracker(enRangeIndexTracker&& other) = default;
+  ~enRangeIndexTracker() = default;
+
+public:
+
+  enRangeIndexTracker& operator = (const enRangeIndexTracker& other) = delete;
+  enRangeIndexTracker& operator = (enRangeIndexTracker&& other) = default;
+
+  /**
+  * @brief : find and gets the first Unused index.
+  * @returns -1 if there are no indexes available.
+  * @param[in] registerTheIndex : to know if the index should be registered if 
+  * one is available found.
+  * @bug : no known bugs.
+  */
+  [[nodiscard]] integralType
+  getFirstUnusedIndex(bool registerTheIndex) 
+  {
+    for( auto& index : m_indexes )
+    {
+      if( false == index.m_isUsed )
+      {
+        if( true == registerTheIndex )
+          index.m_isUsed = true;
+
+        return index.m_index;
+      }
+    }
+
+    return static_cast< integralType >(-1);
+  }
+
+  /**
+  * @brief : registers the index as being used.
+  * @param[in] indexValue : the value that's going to be registered.
+  * @returns : true when the value was registered, false otherwise.
+  * @bug : no known bugs.
+  */
+  [[nodiscard]] bool
+  registerIndexAsUsed(integralType indexValue)
+  {
+
+    for( auto& index : m_indexes )
+    {
+      if( index.m_index == indexValue && false == index.m_isUsed )
+      {
+        index.m_isUsed = true;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+  * @brief : registers the index as available to use again.
+  * @param[in] indexValue : the index thats going to be registered.
+  * @returns  true when a index is successfully registered, false otherwise.
+  * @bug :no known bugs.
+  */
+  [[nodiscard]] bool
+  registerIndexAsAvailable(integralType indexValue)
+  {
+
+    for( auto& index : m_indexes )
+    {
+
+      if( indexValue == index.m_index && true == index.m_isUsed )
+      {
+        index.m_isUsed = false;
+        return true;
+      }
+
+    }
+
+    return false;
+  }
+
+  /**
+  * @returns :the size of the range.
+  * @bug : no known bugs.
+  */
+  size_t 
+  getSizeOfRange() const
+  {
+    return sizeOfRange;
+  }
+
+
+private:
+  /**
+  * @brief : contains all the indexes.
+  */
+  std::array< IndexType, sizeOfRange> m_indexes;
 };
 
--- Graphcis_Framework_Neon/Graphcis_Framework_Neon/src/main.cpp
+++ Graphcis_Framework_Neon/Graphcis_Framework_Neon/src/main.cpp
@@ -1,8 +1,38 @@
 #include "appGraphics.h"
+#include "util/enRangeIndexTarcker.h "
 
 int
 main()
 {
+
+  enRangeIndexTracker<long, 3> instance;
+
+  auto testIndex0 = instance.getFirstUnusedIndex(true);
+  auto testIndex1 = instance.getFirstUnusedIndex(true);
+  auto testIndex2 = instance.getFirstUnusedIndex(false);
+  bool isSuccessful = instance.registerIndexAsUsed(testIndex2);
+
+  assert(isSuccessful == true);
+  isSuccessful = instance.getFirstUnusedIndex(false) == -1;
+  assert(isSuccessful == true);
+
+  isSuccessful = instance.registerIndexAsAvailable(testIndex0);
+  assert(isSuccessful == true);
+  isSuccessful = instance.registerIndexAsAvailable(testIndex1);
+  assert(isSuccessful == true);
+
+  testIndex0 = instance.getFirstUnusedIndex(false);
+  isSuccessful = instance.registerIndexAsUsed(testIndex0);
+  assert(isSuccessful == true);
+  testIndex1 = instance.getFirstUnusedIndex(false);
+  isSuccessful = instance.registerIndexAsUsed(testIndex1);
+  assert(isSuccessful == true);
+
+  isSuccessful = instance.registerIndexAsUsed(-1);
+  assert(isSuccessful == false );
+
+  isSuccessful = instance.registerIndexAsUsed(instance.getSizeOfRange());
+  assert(isSuccessful == true);
   std::unique_ptr<cBaseApp> app = std::make_unique<appGraphics>();
   if( app->init() )
   {
