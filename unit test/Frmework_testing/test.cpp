e0a5cef02c1e62dd197d2976c2c4cccc1c9fddaf
YB <whyis2cool@gmail.com>
2020-04-18 22:25:01 -0500

    Init Test.

---
+++ unit test/Frmework_testing/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+//#include "../../Graphcis_Framework_Neon/Graphcis_Framework_Neon/include/util/enRangeIndexTarcker.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+//tracker
